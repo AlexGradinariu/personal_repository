32b82eed695043dd5a7b790b2511d7e1dee5b505
Gradinariu <alexandru-constantin.gradinariu@continental-corporation.com>
2024-08-21 12:00:58 +0300

    'final_variant'

--- Own_projects/Autovit_scraper/autovit_scraper.py
+++ Own_projects/Autovit_scraper/autovit_scraper.py
@@ -1,8 +1,10 @@
+import os.path
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
-def get_url_info(url):
+def create_request_session(url):
     html_text = requests.get(url)
     if html_text.status_code == 200:
         print(f"Success, responde code : {html_text.status_code}")
@@ -57,22 +59,22 @@ def get_car_details(soup):
         else:
             car_data['Fuel'] = 'None'
         #Extract ad unique id
-        unique_id = ad.get('data-id')
-        if unique_id:
-            car_data['Unique_id'] = unique_id
+        car_id = ad.get('data-id')
+        if car_id:
+            car_data[id_column_title] = car_id
         else:
-            car_data['Unique_id'] = 0
+            car_data[id_column_title] = 0
         car_listings.append(car_data)
     return car_listings
 
-def get_total_pages(soup):
+def get_total_pages_of_an_ad(soup):
     pagination = soup.find('ul', class_='pagination-list')
     if pagination:
         pages = pagination.find_all('li')
         return int(pages[-2].get_text().strip())  # Get the second last item as it contains the total pages
     return 1
 
-def sort_vehicles(list_of_items):
+def filter_and_sort_function(list_of_items):
     min_year = 2019
     min_price = 18000
     max_price = 20000
@@ -85,58 +87,86 @@ def sort_vehicles(list_of_items):
     print(f'{len(sorted_filtered_vehicles)} Vehicles found matching your filters !')
     return sorted_filtered_vehicles
 
-def iterage_throug_all_site_pages(web_site):
+def iterage_throug_ad_pages(web_site):
     print(f'Found a total number of {no_of_site_pages_with_ads} of autovit ads for {web_site}!')
     for page_num in range(1, no_of_site_pages_with_ads + 1):
         page_url = f"{web_site}?page={page_num}"
         print(f"Scraping page {page_num} of {web_site}")
-        page_soup_session = get_url_info(page_url)
+        page_soup_session = create_request_session(page_url)
         car_listings = get_car_details(page_soup_session)
         all_car_listings.extend(car_listings)
 
-def create_excel_file(data,excel):
-    df = pd.DataFrame(data)
-    df.to_excel(excel,index=False)
+def handle_excel_data_sheet(data, excel):
+    if os.path.exists(excel):
+        print("Excel file existing,reading now...")
+        known_df = pd.read_excel(excel)
+        new_df = pd.DataFrame(data)
+        #compare if a new car posting exists or not
+        if isinstance(new_df, pd.DataFrame) and not new_df.empty:
+            known_df[id_column_title] = pd.to_numeric(known_df[id_column_title], errors='coerce').astype('Int64')
+            known_cars = known_df[id_column_title].tolist()
+            new_df[id_column_title] = pd.to_numeric(new_df[id_column_title], errors='coerce').astype('Int64')
+            filtered_new_df = new_df[~new_df[id_column_title].isin(known_cars)]
+            if not filtered_new_df.empty:
+                print('New cars found !')
+                updated_df = pd.concat([known_df, filtered_new_df], ignore_index=True)
+                updated_df.to_excel(excel, index=False)
+                new_cars_notif = dict(updated_df.iloc[:, :2].values)
+                send_notification('New cars found :', new_cars_notif)
+            else:
+                print('No new item found !')
+            # Check for price decreases
+            merged_df = pd.merge(known_df, new_df, on=id_column_title, how='left', suffixes=('_old', '_new'))
+            merged_df['Price_Decreased'] = merged_df['Price_new'] < merged_df['Price_old']
+            # Filter rows where the price has decreased
+            price_decreased_df = merged_df[merged_df['Price_Decreased'] == True]
+            price_decreased_df.to_excel(r'E:\GIT_HUB\automotive_repo\Own_projects\Autovit_scraper\Excel_decreased.xlsx')
+            if not price_decreased_df.empty:
+                print('Price decreases found:')
+                # Optionally, update the known_df with the new (decreased) prices
+                known_df.update(price_decreased_df[[id_column_title, 'Price_new']].rename(columns={'Price_new': 'Price'}))
+                # Save the updated DataFrame back to Excel
+                selected_columns =price_decreased_df[['Title_old','Price_old','Price_new','url_new']]
+                price_change_notif = selected_columns.set_index('Title_old').T.to_dict('list')
+                send_notification('Price change for :', price_change_notif)
+                known_df.to_excel(excel, index=False)
+            else:
+                print('No price decreases found.')
+        else :
+            print("Empty DF !")
+    else:
+        print("Excel file not existing,creating now...")
+        new_cars_notif = dict(pd.iloc[:, :2].values)
+        send_notification('New cars found :', new_cars_notif)
+        new_df = pd.DataFrame(data)
+        new_df.to_excel(excel, index=False)
 
-
-def send_notification(cars):
+def send_notification(notification,cars):
     token = "aqioije5jiex877tdp7fbbvhhd5eeh"
     user_key = "unjez5wivue53eb7ekfhyxrcxvavm6"
 
     url = "https://api.pushover.net/1/messages.json"
-    car_urls = {}
-    for item in cars:
-        car_urls[item['Title']] = item['url']
-    message = (f"Number of vehicles found: {len(sorted_vehicles)}\n"
-                f"Car links : {car_urls}")
+    message = (f"{notification}"
+                f"Car links : {cars}")
     data = {
         "token": token,
         "user": user_key,
         "message": message
     }
-
     requests.post(url, data=data)
 
-def get_car_id(excel):
-    data = pd.read_excel(excel)
-    df = data['Unique_id']
-    if not df.empty:
-        return list(df)
-    else:
-        return 0
 
 if __name__ == "__main__":
-    excel_file = r'E:\GIT_HUB\automotive_repo\Own_projects\Autovit_scraper\Excel.xlsx'
-    cars_to_find = ['/skoda/superb','/volkswagen/passat']
-    base_url = 'https://www.autovit.ro/autoturisme'
-    autovit_site = [base_url + item for item in cars_to_find]
-    all_car_listings = []
-    for web_site in autovit_site:
-        request_session = get_url_info(web_site)
-        no_of_site_pages_with_ads = get_total_pages(request_session)
-        iterage_throug_all_site_pages(web_site)
-    create_excel_file(sorted_vehicles := sort_vehicles(all_car_listings), excel_file)
-    known_vehicles = get_car_id(excel_file)
-    send_notification(sorted_vehicles)
-
-
+    while True:
+        id_column_title = 'Unique_id'
+        Excel_sheet_location = r'E:\GIT_HUB\automotive_repo\Own_projects\Autovit_scraper\Excel.xlsx'
+        cars_to_find = ['/volkswagen/passat','/volkswagen/tiguan','/skoda/superb','/audi/a6']
+        autovit_site = ['https://www.autovit.ro/autoturisme' + item for item in cars_to_find]
+        all_car_listings = []
+        for web_site in autovit_site:
+            request_session = create_request_session(web_site)
+            no_of_site_pages_with_ads = get_total_pages_of_an_ad(request_session)
+            iterage_throug_ad_pages(web_site)
+        handle_excel_data_sheet(sorted_vehicles := filter_and_sort_function(all_car_listings), Excel_sheet_location)
+        print('Waiting 20 seconds till next iteration ...')
+        time.sleep(7200)
