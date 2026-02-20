edb170100396421d26e566cffe976a0a6d9de24c
Alexandru-Constantin Gradinariu <alexandru-constantin.gradinariu@continental-corporation.com>
2026-02-20 17:21:16 +0200

    improved subtitle

--- Own_projects/Subs_checker/subtitle_checker.py
+++ Own_projects/Subs_checker/subtitle_checker.py
@@ -6,37 +6,35 @@ def check_subtitle_files(directory):
     sub_files = []
     subtitle_extensions = ('.srt', '.sub')
     if os.path.exists(directory):
-        for root,_, filename in os.walk(directory):
-            for file in filename:
-                if file.lower().endswith(subtitle_extensions):
-                    sub_files.append(os.path.join(root, file))
+        for file in os.listdir(directory):
+            if file.lower().endswith(subtitle_extensions):
+                file_path = os.path.join(directory, file)
+                sub_files.append(file_path)
     return sub_files
 
 def replace_broken_letter(subs_files: list):
+    files_modified = []
     dictionary = {"º":"s",
                   "þ":"t",
                   "Þ":"t",
                   "ª":"s",}
     for file in subs_files:
-        flag = False
+        print('working on: ', file)
         try:
             with open(file, 'r',encoding='utf-8') as f:
                 content = f.read()
                 for key, value in dictionary.items():
                     if key in content:
-                        print('Found broken letter:', key, 'in file:', file)
-                        flag = True
                         content=content.replace(key, value)
-            if flag:
-                with open(file, 'w', encoding='utf-8') as f:
-                    f.write(content)
-                    print(f"Modified file{file}")
-            else:
-                print(f"No broken letters found in file {file}")
+                        files_modified.append(file)
+            with open(file, 'w',encoding='utf-8') as f:
+                f.write(content)
         except Exception as e:
             print(f"Error processing file {file}: {e}")
-
+    if files_modified:
+        print("file modified: ", files_modified,end="")
 if __name__=="__main__":
     directory = input("Enter the directory path: ")
     files = check_subtitle_files(directory)
     replace_broken_letter(files)
+    print("Done")
